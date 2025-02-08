af7db4f7af2669dd42673a5ffe4fce6c48d93cb1
Oleg Bryukhanov <olegbrukhanov@yandex.ru>
2025-02-08 19:06:17 +0300

    testing flake8

--- backend/recipes/management/commands/add_data.py
+++ backend/recipes/management/commands/add_data.py
@@ -2,7 +2,6 @@ import json
 
 from django.core.management.base import BaseCommand
 
-from api.users.serializers import User
 from recipes.models import Ingredient, Tag
 
 
@@ -30,15 +29,3 @@ class Command(BaseCommand):
         self.stdout.write(self.style.SUCCESS(
             'Successfully loaded ingredients'))
 
-        # with open('data/users.json') as f:
-        #     users_data = json.load(f)
-        #     for user_ in users_data:
-        #         User.objects.get_or_create(
-        #             username=user_['username'],
-        #             email=user_['email'],
-        #             first_name=user_['first_name'],
-        #             last_name=user_['last_name'],
-        #             role=user_['role'],
-        #             password=user_['password'],
-        #         )
-        # self.stdout.write(self.style.SUCCESS('Successfully loaded users'))
