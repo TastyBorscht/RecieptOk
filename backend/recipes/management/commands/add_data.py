cae8280a5a29474581b03adee8cb0aa938b03879
Oleg Bryukhanov <olegbrukhanov@yandex.ru>
2025-02-11 05:30:58 +0300

    some changes

--- backend/recipes/management/commands/add_data.py
+++ backend/recipes/management/commands/add_data.py
@@ -7,6 +7,9 @@ class Inrgredient:
     pass
 
 
+#  не смотря на bulk_create на удаленном сервере эта команда выбрасывает ошибку
+#  django.***.utils.DataError: value too long for type character varying(50)
+# 2025/02/11 02:26:27 Process exited with status 1
 class Command(BaseCommand):
     help = 'Load data from recipe.json and tags.json'
 
@@ -25,7 +28,7 @@ class Command(BaseCommand):
         with open('data/ingredients.json') as f:
             ingredients_data = json.load(f)
             ingredients_to_create = [
-                Ingredient(name=ingredient['name'],  # Исправлено
+                Ingredient(name=ingredient['name'],
                            measurement_unit=ingredient['measurement_unit'])
                 for ingredient in ingredients_data
             ]
