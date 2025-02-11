50e7edf2fc8a9d1ee4e743514e61ff1979c2edee
Oleg Bryukhanov <olegbrukhanov@yandex.ru>
2025-02-11 15:29:13 +0300

    isort 666

--- backend/api/users/urls.py
+++ backend/api/users/urls.py
@@ -1,12 +1,11 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import CustomUserViewSet, LegendAvatarView
+from .views import CustomUserViewSet
 
 user_router = routers.DefaultRouter()
 user_router.register('', CustomUserViewSet, basename='users')
 
 urlpatterns = [
-    # path('me/avatar/', LegendAvatarView.as_view(), name='avatar'),
     path('', include(user_router.urls)),
 ]
