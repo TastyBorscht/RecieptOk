683ba11e7a69e85d0b62f64a23b38e670787404f
Oleg Bryukhanov <olegbrukhanov@yandex.ru>
2025-02-12 21:40:02 +0300

    remove wrong about

--- backend/pages/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from pages import views
-
-app_name = 'pages'
-
-urlpatterns = [
-    path('', views.About.as_view(), name='about'),
-]
--- backend/pages/views.py
+++
@@ -1,5 +0,0 @@
-from django.views.generic import TemplateView
-
-
-class About(TemplateView):
-    template_name = 'templates/pages/about.html'
