b7e95d9d20546e3fdd0ea78d130a412e5163e7b5
Oleg Bryukhanov <olegbrukhanov@yandex.ru>
2025-02-13 00:31:29 +0300

    test

--- backend/mypages/views.py
+++ backend/mypages/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class About(TemplateView):
     template_name = 'templates/pages/about.html'
+
+
+class Technologies(TemplateView):
+    template_name = 'templates/pages/technologies.html'
