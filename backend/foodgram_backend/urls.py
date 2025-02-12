2c0dfb5fb7f05c812b1491aa028b63373d34b77f
Oleg Bryukhanov <olegbrukhanov@yandex.ru>
2025-02-13 00:32:25 +0300

    trying to add about and tech

--- backend/foodgram_backend/urls.py
+++ backend/foodgram_backend/urls.py
@@ -5,6 +5,7 @@ from django.urls import include, path
 
 from mypages.views import About, Technologies
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
