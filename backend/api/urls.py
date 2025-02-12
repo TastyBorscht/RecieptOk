aa5227ecb76dd88c359852c1be12da2a0ebc9e1d
Oleg Bryukhanov <olegbrukhanov@yandex.ru>
2025-02-12 16:18:17 +0300

    trying fix

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('recipes/', include('api.recipes.urls')),
     path('tags/', include('api.tags.urls')),
     path('users/', include('api.users.urls')),
-    path('', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+    path('auth/token/', include('djoser.urls.authtoken')),
+    path('', include(api_router.urls)),
 ]
