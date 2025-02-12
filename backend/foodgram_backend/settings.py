77b7c98ca3ba6d67080f0c838ff6dc0b39cefe45
Oleg Bryukhanov <olegbrukhanov@yandex.ru>
2025-02-12 22:23:18 +0300

    data fixed

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -61,6 +61,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'foodgram_backend.wsgi.application'
 
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', 5432)
+    }
+}
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
